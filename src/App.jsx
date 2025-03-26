3f4cf9dae02a3f31f71cdc3a4c10440de6dd5d98
arron-dunne <arrondunneps@gmail.com>
2025-03-26 16:01:57 +1100

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
     
     return (
-        <div>
+        <div className="w-screen h-screen box-border flex items-center justify-center font-bold text-xl bg-red-300">
             <p>Welcome to Finance Tracker!</p>
         </div>
     )
