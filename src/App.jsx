35d4c2038d1aabf9d95736be9f341d7e0112799e
arron-dunne <arrondunneps@gmail.com>
2025-03-26 16:29:29 +1100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 
 function App() {
-    
+
     return (
-        <div className="w-screen h-screen box-border flex items-center justify-center font-bold text-xl bg-red-300">
-            <p>Welcome to Finance Tracker!</p>
+        <div className="w-screen h-screen box-border">
+            <Navbar />
+        </div>
+    )
+}
+
+function Navbar() {
+    return (
+        <div className="bg-blue-400 w-full p-6 flex items-center justify-between">
+            <p className="text-3xl text-center text-white font-bold">Finance Tracker</p>
+            <Links />
+        </div>
+    )
+}
+
+function Links() {
+    return (
+        <div className="flex gap-4">
+            <Link name='Dashboard'/>
+            <Link name='Breakdown'/>
+            <Link name='Compare'/>
+            <Link name='Transactions'/>
+        </div>
+    )
+}
+
+function Link({name}) {
+    return (
+        <div className="bg-blue-100 text-blue-500 rounded-full px-3 py-1">
+            {name}
         </div>
     )
 }
