7326b300609daedc4619f4f9ee5a99a926cc1edb
arron-dunne <arrondunneps@gmail.com>
2025-03-28 11:54:00 +1100

    trying to impot data from json

--- src/Transactions.jsx
+++ src/Transactions.jsx
@@ -1,7 +1,29 @@
+import data from './data.json'
+
 export function Transactions() {
+
     return (
         <div>
             Transactions page
+            { data.date }
+            {/* <table className="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2">Date</th>
+                        <th className="px-4 py-2">Description</th>
+                        <th className="px-4 py-2">Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map((transaction, index) => (
+                        <tr key={index}>
+                            <td className="border px-4 py-2">{transaction.date}</td>
+                            <td className="border px-4 py-2">{transaction.description}</td>
+                            <td className="border px-4 py-2">{transaction.amount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table> */}
         </div>
     )
 }
