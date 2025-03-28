12f2c24bed2c49634bc9def453e621388d682166
arron-dunne <arrondunneps@gmail.com>
2025-03-28 14:03:38 +1100

    added transaction data

--- src/Transactions.jsx
+++ src/Transactions.jsx
@@ -3,27 +3,37 @@ import data from './data.json'
 export function Transactions() {
 
     return (
-        <div>
-            Transactions page
-            { data.date }
-            {/* <table className="table-auto w-full">
-                <thead>
-                    <tr>
-                        <th className="px-4 py-2">Date</th>
-                        <th className="px-4 py-2">Description</th>
-                        <th className="px-4 py-2">Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {transactions.map((transaction, index) => (
-                        <tr key={index}>
-                            <td className="border px-4 py-2">{transaction.date}</td>
-                            <td className="border px-4 py-2">{transaction.description}</td>
-                            <td className="border px-4 py-2">{transaction.amount}</td>
+        <div className='p-6'>
+            <div className='text-2xl pl-3 pb-1 w-full border-b'>
+                <p>All Transactions</p>
+            </div>
+            {/* <table className="table-auto w-full"> */}
+            <div className='flex justify-center mt-8'>
+                <table className='rounded-md'>
+                    <thead className='bg-gray-200 border'>
+                        <tr>
+                            <th className="px-4 py-2">Date</th>
+                            <th className="px-4 py-2">Account</th>
+                            <th className="px-4 py-2">Currency</th>
+                            <th className="px-4 py-2">Description</th>
+                            <th className="px-4 py-2">Category</th>
+                            <th className="px-4 py-2">Amount</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table> */}
+                    </thead>
+                    <tbody>
+                        {data.map(transaction => (
+                            <tr className='border'>
+                                <td className="px-4 py-2">{transaction.date}</td>
+                                <td className="px-4 py-2">{transaction.account}</td>
+                                <td className="px-4 py-2">{transaction.currency}</td>
+                                <td className="px-4 py-2">{transaction.description}</td>
+                                <td className="px-4 py-2">{transaction.category}</td>
+                                <td className="px-4 py-2">{transaction.amount}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
