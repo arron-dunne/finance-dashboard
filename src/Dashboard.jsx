5e9c551218c27c649b974a7500582881972ee11f
arron-dunne <arrondunneps@gmail.com>
2025-04-04 10:49:04 +1100

    sorting and totaling

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,20 +5,20 @@ export function Dashboard() {
 
     // Sum category spending
     const income = []
-    
+
     const outgoings = []
 
     data.forEach(transaction => {
 
         let chart = outgoings
-        
+
         if (transaction.amount > 0) {
             chart = income
         }
 
         if (chart.find(cat => cat.label === transaction.category)) {
             chart.find(cat => cat.label === transaction.category).value += transaction.amount
-        } 
+        }
         else {
             chart.push({
                 label: transaction.category,
@@ -27,51 +27,100 @@ export function Dashboard() {
         }
     })
 
-    // round to 2 decimal places
+    // sort
+    income.sort((a, b) => b.value - a.value)
+    outgoings.sort((a, b) => b.value - a.value)
+
+    // total
+    const incomeTotal = income.reduce((accumulator, currentValue) => accumulator + currentValue.value, 0)
+    const outgoingsTotal = outgoings.reduce((accumulator, currentValue) => accumulator + currentValue.value, 0)
+    const netTotal = incomeTotal + outgoingsTotal
+
+    const totals = { incomeTotal, outgoingsTotal, netTotal }
+
+    // round
     income.forEach(cat => {
         cat.value = cat.value.toFixed(2)
     })
-    
+
     outgoings.forEach(cat => {
         cat.value = (-cat.value).toFixed(2)
     })
 
-    // sort by value
-    income.sort((a, b) => b.value - a.value)
-    outgoings.sort((a, b) => b.value - a.value)
-
     return (
-        <div className='p-8 flex justify-center gap-8 md:gap-20'>
-            <div className='flex flex-col gap-8 items-center'>
-                <p className='text-lg font-bold underline'>Income</p>
-                <div className='w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96'>
-                    <PieChart
-                        series={[{ data: income }]}
-                        slotProps={{ legend: { hidden: true}}}
-                        margin={{
-                            left: 0,
-                            right: 0,
-                            top: 0,
-                            bottom: 0,
-                        }}
-                    />
+        <div className='p-8'>
+            <div className='flex justify-center gap-8 md:gap-20 mb-10'>
+                <div className='flex flex-col gap-8 items-center'>
+                    <p className='text-lg font-bold underline'>Income</p>
+                    <div className='w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96'>
+                        <PieChart
+                            series={[{ data: income }]}
+                            slotProps={{ legend: { hidden: true } }}
+                            margin={{
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                            }}
+                        />
+                    </div>
+                </div>
+                <div className='flex flex-col gap-8 items-center'>
+                    <p className='text-lg font-bold underline'>Outgoings</p>
+                    <div className='w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96'>
+                        <PieChart
+                            series={[{ data: outgoings }]}
+                            slotProps={{ legend: { hidden: true } }}
+                            margin={{
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
-            <div className='flex flex-col gap-8 items-center'>
-                <p className='text-lg font-bold underline'>Outgoings</p>
-                <div className='w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96'>
-                    <PieChart
-                        series={[{ data: outgoings }]}
-                        slotProps={{ legend: { hidden: true}}}
-                        margin={{
-                            left: 0,
-                            right: 0,
-                            top: 0,
-                            bottom: 0,
-                        }}
-                    />
-                </div>
+            <div className='flex justify-center'>
+                <DashboardTable income={income} outgoings={outgoings} totals={totals} />
             </div>
         </div>
+    )
+}
+
+function DashboardTable({ income, outgoings, totals }) {
+    return (
+        <table>
+            <tbody>
+                <tr className='border'>
+                    <td className='px-4 py-2 font-bold'>Income</td>
+                    <td className='px-4 py-2 font-bold'>{Intl.NumberFormat('en-US', { style: "currency", currency: "USD" }).format(totals.incomeTotal.toFixed(2))}</td>
+                </tr>
+                {income.map((transaction, index) => (
+                    <tr key={index} className='border'>
+                        <td className="px-4 py-2">{transaction.label}</td>
+                        <td className="px-4 py-2">{Intl.NumberFormat('en-US', { style: "currency", currency: "USD" }).format(transaction.value)}</td>
+                    </tr>
+                ))}
+
+                <tr className='border'>
+                    <td className='px-4 py-2 font-bold'>Outgoings</td>
+                    <td className='px-4 py-2 font-bold'>{Intl.NumberFormat('en-US', { style: "currency", currency: "USD" }).format(totals.outgoingsTotal.toFixed(2))}</td>
+                </tr>
+                {outgoings.map((transaction, index) => (
+                    <tr key={index} className='border'>
+                        <td className="px-4 py-2">{transaction.label}</td>
+                        <td className="px-4 py-2">{Intl.NumberFormat('en-US', { style: "currency", currency: "USD" }).format(transaction.value)}</td>
+
+                    </tr>
+                ))}
+
+                <tr className='border'>
+                    <td className='px-4 py-2 font-bold'>Total</td>
+                    <td className='px-4 py-2 font-bold' style={{ 'color': totals.netTotal > 0 ? 'green' : 'red'}}>{Intl.NumberFormat('en-US', { style: "currency", currency: "USD" }).format(totals.netTotal.toFixed(2))}</td>
+                </tr>
+
+            </tbody>
+        </table>
     )
 }
